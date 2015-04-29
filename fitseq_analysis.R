cf7042097724fe3adc63101478593f84a8e4a67e
aviv <aviv@genomecompiler.com>
2015-04-29 11:43:26 +0300

    changed entire script to work with dplyr, still need to get scatter matrix to work

--- fitseq_analysis.R
+++ fitseq_analysis.R
@@ -1,153 +1,150 @@
-anc.1.freq = read.csv("C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\data\\prelim_fitseq_results_130515\\fitseq_sample_data_result_1_anc.csv")
+require(dplyr)
+require(ineq)
+
+results.dir = 'C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\results\\'
+
+raw_data_location = 'C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\data\\final_result_24-04_changed_label.csv'
+fitseq_raw_data = read.csv(raw_data_location,check.names=FALSE)
 
 
-anc.1.freq[which(as.numeric(anc.1.freq[,2]) == 0),]
-hist(log10(as.numeric(anc.1.freq[,2])))
+by_evo_time_fitseq_data  <- fitseq_raw_data %>% 
+  select(-design ,anc_1, anc_2, A_4, B_4,C_4,D_4,E_4,F_4, A_8, B_8,C_8,D_8,E_8,F_8
+         , A_12, B_12,C_12,D_12,E_12,F_12, A_16, B_16,C_16,D_16,E_16,F_16, A_20, B_20,C_20,D_20,E_20,F_20
+         , A_24, B_24,C_24,D_24,E_24,F_24, A_28, B_28,C_28,D_28,E_28,F_28)
 
-fitseq.all.raw = read.csv("C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\data\\prelim_fitseq_results_130515\\all_fitseq.csv",row.names=1)
-fitseq.all.raw[fitseq.all.raw$X1_anc>50000,] <-  0
+by_line_fitseq_data  <- 
+  fitseq_raw_data %>% 
+  select(-design ,anc_1, anc_2, A_4,A_8,A_12,A_16,A_20,A_24,A_28,B_4,B_8,B_12,B_16,B_20,B_24,B_28,
+         C_4,C_8,C_12,C_16,C_20,C_24,C_28,D_4,D_8,D_12,D_16,D_20,D_24,D_28,
+         E_4,E_8,E_12,E_16,E_20,E_24,E_28,F_4,F_8,F_12,F_16,F_20,F_24,F_28)
 
-
-plot(fitseq.all.raw[,'X1_anc'],fitseq.all.raw[,'X2_anc']*0.22, ylim = c(0,300), xlim = c(0,300))
-
-plot(fitseq.all[which(as.numeric(fitseq.all[,3]) <10  ) ,2],fitseq.all[which(as.numeric(fitseq.all[,3]) <10  ) ,3]* 0.22 )
-
-
-as.numeric(fitseq.all[13365,2])
-
-fitseq.all[13365,2] = 0
-
-fitseq.all[which(as.numeric(fitseq.all[,2]) >300) ,2] = 0
-
-results.dir = 'C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\results\\initial_data_analyses\\'
-
-
-png(paste0(results.dir,'initial_timepoint_vs_ancestor.png'),units="in", width=15, height=15, res=100)
-
-par(mfrow = c(7,2))
-
-for (i in 1:7){
-  
-  plot(fitseq.all.raw[,'X1_anc']/sum(fitseq.all.raw[,'X1_anc']),fitseq.all.raw[,3+i]/sum(fitseq.all.raw[,3+i]), 
-       xlab = 'Ancestor 1', ylab = paste('Timepoint',colnames(fitseq.all.raw)[3+i] ))
-  abline(0,1)
-  
-  plot(fitseq.all.raw[,'X2_anc']/sum(fitseq.all.raw[,'X2_anc']),fitseq.all.raw[,3+i]/sum(fitseq.all.raw[,3+i]), 
-       xlab = 'Ancestor 2', ylab = paste('Timepoint',colnames(fitseq.all.raw)[3+i] ))
-  
-  abline(0,1)
-  
-  
+fitseq_raw_data_no_design  <- fitseq_raw_data  %>% select(-design)
+design_list = vector("list")
+freq_list = vector("list")
+fitseq_a_series_no_design <- fitseq_a_series  %>% select(-design)
+for (i in 1:length(fitseq_a_series_no_design)){
+  name  <- colnames(fitseq_a_series_no_design)[i]
+  current_column  <- fitseq_a_series[,c('design',name)]
+  ordered_current_column = current_column[order(current_column[,2],decreasing = T),]
+  design_list = c(design_list,as.list(as.character(head(ordered_current_column,3)[,1]))))
+freq_list = c(design_list,as.list(as.numeric(head(ordered_current_column,3)[,2])))
 }
+print 
+
+
+
+fitseq_a_series  <- fitseq_raw_data %>% select(design,A_4,A_8,A_12,A_16,A_20,A_24,A_28)
+fitseq_b_series  <- fitseq_raw_data %>% select(design,B_4,B_8,B_12,B_16,B_20,B_24,B_28)
+fitseq_c_series  <- fitseq_raw_data %>% select(design,C_4,C_8,C_12,C_16,C_20,C_24,C_28)
+fitseq_d_series  <- fitseq_raw_data %>% select(design,D_4,D_8,D_12,D_16,D_20,D_24,D_28)
+fitseq_e_series  <- fitseq_raw_data %>% select(design,E_4,E_8,E_12,E_16,E_20,E_24,E_28)
+fitseq_f_series  <- fitseq_raw_data %>% select(design,F_4,F_8,F_12,F_16,F_20,F_24,F_28)
+fitseq_ancestors  <- fitseq_raw_data %>% select(design,anc_1, anc_2)
+
+a_variants_over_time   <- 
+  fitseq_a_series %>% select(-design)  %>%  summarise_each(funs(sum(.>0)/14234))
+
+a_variants_over_time   <- 
+  fitseq_a_series %>% select(-design)  %>%  summarise_each(funs(sum(.>0)/14234))
+
+a_variants_over_time   <- 
+  fitseq_a_series %>% select(-design)  %>%  summarise_each(funs(sum(.>0)/14234))
+
+a_variants_over_time   <- 
+  fitseq_a_series %>% select(-design)  %>%  summarise_each(funs(sum(.>0)/14234))
+
+a_variants_over_time   <- 
+  fitseq_a_series %>% select(-design)  %>%  summarise_each(funs(sum(.>0)/14234))
+
+a_variants_over_time   <- 
+  fitseq_a_series %>% select(-design)  %>%  summarise_each(funs(sum(.>0)/14234))
+
+
+
+png(paste0(results.dir,'a_series_scatter_matrix.png'),units="in", width=20, height=20, res=100)
+
+pairs(merge(fitseq_ancestors,fitseq_a_series,by='design')[,-1])
 dev.off()
 
 
+fitseq_tp_4  <- fitseq_raw_data %>% select(design,A_4,B_4,C_4,D_4,E_4,F_4)
+fitseq_tp_8  <- fitseq_raw_data %>% select(design,A_8,B_8,C_8,D_8,E_8,F_8)
+fitseq_tp_12  <- fitseq_raw_data %>% select(design,A_12,B_12,C_12,D_12,E_12,F_12)
+fitseq_tp_16  <- fitseq_raw_data %>% select(design,A_16,B_16,C_16,D_16,E_16,F_16)
+fitseq_tp_20  <- fitseq_raw_data %>% select(design,A_20,B_20,C_20,D_20,E_20,F_20)
+fitseq_tp_24  <- fitseq_raw_data %>% select(design,A_24,B_24,C_24,D_24,E_24,F_24)
+fitseq_tp_28  <- fitseq_raw_data %>% select(design,A_28,B_28,C_28,D_28,E_28,F_28)
 
-png(paste0(results.dir,'initial_timepoint_vs_timepoint.png'),units="in", width=15, height=15, res=100)
+#clustering
+#spearman
 
-par(mfrow = c(7,7))
+fitseq_spearman_distance_matrix  <-  dist(1-cor(fitseq_raw_data %>% select(-design), method = "spearman"))
 
-for (i in 1:7){
-  for (j in 1:7){
-    
-    plot(fitseq.all.raw[,1+i]/sum(fitseq.all.raw[,1+i]),fitseq.all.raw[,1+j]/sum(fitseq.all.raw[,1+j]), 
-         xlab = paste('Timepoint',colnames(fitseq.all.raw)[1+i] ), ylab = paste('Timepoint',colnames(fitseq.all.raw)[1+j] ))
-    abline(0,1)
-    
-    
-  }
-}
+fitseq_spearman_clustering  <- hclust(fitseq_spearman_distance_matrix,method = 'average')
+
+png(paste0(results.dir,'fitseq_spearman_upgma_clustering.png'),units="in", width=20, height=10, res=100)
+
+plot(fitseq_spearman_clustering)
+
 dev.off()
 
-days = c(0,4,8,12,16,20,24,28)
-day_values = vector(length = 10)
 
-for (i in c(2,4:10)){
-  print(i)
-  print(fitseq.all.raw[1,i])
-  print(sum(fitseq.all.raw[,i]))
-  day_values[i] = as.numeric(fitseq.all.raw[1,i])/as.numeric(sum(fitseq.all.raw[,i]))
-  print(day_values[i])
-  print(day_values)
-  
-}
-plot(days,day_values[c(2,4:10)])
+write.csv(cor(fitseq_raw_data %>% select(-design), method = "spearman"),
+          paste0(results.dir,'spearman_correlation_matrix.csv'))
 
+#pearson
 
+fitseq_pearson_distance_matrix  <-  dist(1-cor(fitseq_raw_data %>% select(-design), method = "pearson"))
 
+fitseq_pearson_clustering  <- hclust(fitseq_pearson_distance_matrix,method = 'average')
 
+png(paste0(results.dir,'fitseq_pearson_upgma_clustering.png'),units="in", width=20, height=10, res=100)
 
-all_sums = apply(fitseq.all.raw[,3:9],2,sum)
+plot(fitseq_pearson_clustering)
 
-days = c(0,4,8,12,16,20,24,28)
-days_mat =matrix(days,nrow=100,ncol=length(days),byrow=TRUE)
-
-
-random.normal  <-  fitseq.all.raw %>% 
-  select(-X2_anc) %>% 
-sample_n(100)/all_sums
-
-normal.data  <- fitseq.all.raw %>% 
-   select(-X2_anc)/all_sums
-
-plot(days,normal.data[1,],type = "l", lty = 1)
-
-matplot(t(days_mat),t(normal.data[1:100,]), 
-         ylim = c(0,max(normal.data[1:100,]) ))
-
-matlines(t(days_mat),t(normal.data[1:100,]), 
-        ylim = c(0,max(normal.data[1:100,]) ))
-
-
-random.normal  <-  fitseq.all.raw %>% 
-  select(-X2_anc) %>% 
-  sample_n(100)/all_sums
-
-normal.data  <- fitseq.all.raw %>% 
-  select(-X2_anc)/all_sums
-
-fitseq_lm  <-
-  normal.data %>%
-  mutate(R2 = get.lm.to.days.r2(1:8),slope = get.lm.to.days.slope(1:8)) %>%
-  arrange(slope,R2) %>%
-  select(-slope,-R2)
-
-glimpse(fitseq_lm)
-
-plotnRows(paste0(results.dir,'bottom_100_slopes.png'),100,tail(fitseq_lm,100),
-          "Top 100 R^2 varaints with ascending linear models")
+dev.off()
+write.csv(cor(fitseq_raw_data %>% select(-design), method = "pearson"),
+          paste0(results.dir,'pearson_correlation_matrix.csv'))
 
 
 
-plotnRows  <-  function(figure_name,n,rows,title){
-  days_mat =matrix(days,nrow=n,ncol=length(days),byrow=TRUE)
-  
-  png(figure_name,units="in", width=10, height=7, res=100)
-  
-  print(nrow(days_mat))
-  print(nrow(t(days_mat)))
-  print( nrow(rows))
-  print( nrow(t(rows)))
-  
-  matplot(t(days_mat),t(rows), 
-          ylim = c(0,max(rows) ), ylab = 'DNA coverage normalized by all mapped reads in bin',
-          xlab = 'days',main = title)
-  
-  matlines(t(days_mat),t(rows), 
-           ylim = c(0,rows))
-  
-  dev.off()
-  
-}
+#scatter plots and matrices
+
+library(lattice )
+set = '28'
+
+log10(fitseq_raw_data %>% select(contains(set)))
 
 
-get.lm.to.days.slope  <- function(data){
-  linear = lm(unlist(data)~days)
-  return(coef(linear)[2] )
-}
+# scatter_plot_matrix_for  <-  function(set){
+
+max =  max(fitseq_raw_data %>% select(contains(set)) )
 
 
-get.lm.to.days.r2  <- function(data){
-  linear = lm(unlist(data)~days)
-  return(summary(linear)$adj.r.squared )
-}
+png(paste0(results.dir,'scatter_plot_matrix_',set,'.png'),units="in", width=20, height=10, res=100)
+
+
+splom(log10(fitseq_raw_data %>% select(contains(set)))
+      #   fitseq_raw_data %>% select(contains(set))
+      #       ,xlim =c(0,max),ylim =c(0,max)
+      #      ,scales= list(
+      #              relation = 'same',
+      #                    log = TRUE,equispaced.log = TRUE
+      
+      # #                   ,at = list(max/1000,max/100,max/10,max/2,max)
+      # #                     , labels = list(as.character(max/1000),as.character(max/100),as.character(max/10),
+      # #                                  as.character(max/2),as.character(max)
+      #                                   )
+)
+dev.off()
+# }
+
+max(fitseq_raw_data %>% select(anc_2,anc_1) )
+
+anc_max = max(fitseq_raw_data %>% select(anc_2,anc_1) )
+plot(unlist(fitseq_raw_data %>% select(anc_1)),
+     unlist(fitseq_raw_data %>% select(anc_2)),
+     log ='xy', xlim = c(1,anc_max),ylim = c(1,anc_max))
+abline(0,1)
+cor(fitseq_raw_data %>% select(anc_1),
+    fitseq_raw_data %>% select(B_8))
