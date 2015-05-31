38036129deb2d148c32f5955a48ff85f4b7f3b86
aviv <aviv@genomecompiler.com>
2015-05-31 15:18:52 +0300

    changed names

--- growth_rate_difference.R
+++
@@ -1,6 +0,0 @@
-get.growth.rate.difference  <- function(read.ratio,generations){
-  difference = log2(read.ratio)/generations
-  return(difference)
-}
-
-print(get.growth.rate.difference(5*10^-5,99))
