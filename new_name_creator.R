ac07f6e2ee110a8366b51bfb47660bb74cef2799
aviv <aviv@genomecompiler.com>
2015-05-31 15:19:22 +0300

    fixed bug in rare and delta g 10

--- new_name_creator.R
+++ new_name_creator.R
@@ -1,3 +1,4 @@
+require(gdata)
 n.term = 
   read.csv('C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\data\\n_terminal_data_with_full_seq.csv')
 
@@ -8,15 +9,17 @@ for (i in 1:nrow(n.term)){
   RBS.letter  = toupper(substr(n.term[i,'RBS.Display'],1,2))
   gene.number = as.numeric(n.term[i,'Gene'])
   if (startsWith(n.term[i,'CDS.type'],'Max')){
-    type = 'C'
+    type = 'R'
   } else if (startsWith(n.term[i,'CDS.type'],'Min')){
     type = 'C'
   
 } else if (startsWith(n.term[i,'CDS.type'],'WT')){
   type = 'W'
 } else if (startsWith(n.term[i,'CDS.type'],'גˆ†G')){
-  type = substr(unlist(strsplit(as.character(n.term[i,'CDS.type']),split = ' '))[2],1,1)
+  type = substr(unlist(strsplit(as.character(n.term[i,'CDS.type']),split = ' '))[2],1,2)
 }
 new.name <- append(new.name,paste(promoter.letter,RBS.letter,gene.number,type,sep = '_'))
 
 }
+
+write.csv(cbind(as.character(n.term$Name), new.name),'C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\data\\new_name.csv',quote = F)
