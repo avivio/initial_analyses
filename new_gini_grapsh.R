32a71c549f260dedae6719ff9804a2801eb83ea8
aviv <aviv@genomecompiler.com>
2015-07-23 11:40:15 +0300

    made graphs in 1 line, ordered ancestors, added pre post umi

--- new_gini_grapsh.R
+++ new_gini_grapsh.R
@@ -12,7 +12,6 @@ theme_aviv <-     theme_minimal() +
 
 post.umi.data.location  <- 'C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\data\\clean_data\\final_result_14-05-15-1551_2_mismatches.csv'
 post.umi.data = read.csv(post.umi.data.location)
-colnames(post.umi.data)[40] <- 'C_0'
 post.umi.data  <- post.umi.data %>%   gather(sample, frequency, A_24:F_4)
 post.umi.data  <- post.umi.data %>% separate(sample, c("lineage", "day"), '_',convert = T)
 post.umi.data <- post.umi.data %>%
@@ -20,28 +19,43 @@ post.umi.data <- post.umi.data %>%
   mutate(sum.sample= sum(frequency),norm.freq = frequency/sum.sample)
 
 
-pnas.correction.data.location  <- 'C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\data\\clean_data\\pnas_correction_14-05-15-1551_2_mismatches.csv'
-pnas.correction.data = read.csv(pnas.correction.data.location)
-pnas.correction.data  <- pnas.correction.data %>%   gather(sample, frequency, A_24:F_4)
-pnas.correction.data  <- pnas.correction.data %>% separate(sample, c("lineage", "day"), '_',convert = T)
-pnas.correction.data <- pnas.correction.data %>%
+pre.umi.data.location  <- 'C:\\Users\\dell7\\Documents\\Tzachi\\workspace\\data\\clean_data\\final_match_count_14-05-15-1551_2_mismatches.csv'
+pre.umi.data = read.csv(pre.umi.data.location)
+pre.umi.data  <- pre.umi.data %>%   gather(sample, frequency, A_24:F_4)
+pre.umi.data  <- pre.umi.data %>% separate(sample, c("lineage", "day"), '_',convert = T)
+pre.umi.data <- pre.umi.data %>%
   group_by(day,lineage) %>% 
   mutate(sum.sample= sum(frequency),norm.freq = frequency/sum.sample)
 
 
-# joined_data <- left_join(post.umi.data,pnas.correction.data,by = c('X','lineage', 'day') )
-# gini <- joined_data %>% 
-#   group_by(day,lineage) %>%
-#   summarise(post.umi.freq =  ineq(frequency.x,type="Gini"),pnas.correction.freq =  ineq(frequency.y,type="Gini"))
-# gini <-  gini %>%
-#   gather(data.set, gini,post.umi.freq:pnas.correction.freq)
+joined_data <- left_join(post.umi.data,pre.umi.data,by = c('X','lineage', 'day') )
+gini <- joined_data %>% 
+  group_by(day,lineage) %>%
+  summarise(post.umi.freq =  ineq(frequency.x,type="Gini"),pre.umi.freq =  ineq(frequency.y,type="Gini")) 
+
+# gini <- filter(gini,day !=0)
+# 
+# gini[gini$day == 1,'lineage'] <- 'A'
+# gini[gini$day == 1,'day'] <- 0
+# 
+gini <- filter(gini,lineage !='anc')
+
+gini <-  gini %>%
+  gather(data.set, gini,post.umi.freq:pre.umi.freq)
 # gini <-  gini %>%
 #   filter(data.set == 'gini,post.umi.freq')
-gini <- post.umi.data %>% 
-    group_by(day,lineage) %>%
-    summarise(gini =  ineq(frequency,type="Gini"))
+# gini <- post.umi.data %>% 
+#     group_by(day,lineage) %>%
+#     summarise(gini =  ineq(frequency,type="Gini"))
 
-ggplot(filter(gini,lineage =='B'), aes(y = gini,x = factor(day),fill = 'cyan')) + geom_bar(stat = 'identity',) +
-   ylab('Gini index') + xlab('Day') + guides(fill = guide_legend(title = "Lineage")) + theme_aviv + 
-  ggtitle('Gini index over time lineage B') + guides(fill=FALSE)
+ggplot(gini, aes(y = gini,x = factor(day),fill = data.set)) + 
+  geom_bar(stat = 'identity',position = 'dodge') +
+   ylab('Gini index\n') + 
+  xlab('\nDay') + 
+  scale_fill_discrete(name="Pre or Post\nUMI processing",
+                      breaks=c("pre.umi.freq", "post.umi.freq"),
+                      labels=c("Pre UMI", "Post UMI")) +
+  # guides(fill = guide_legend(title = "Pre or Post\nUMI processing")) +
+  theme_aviv + 
+  ggtitle('Gini index over time lineage B')  + facet_wrap(~lineage,nrow = 1)
   
